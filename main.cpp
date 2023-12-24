a4220450c0bebd02d0cb293f59d6489939396690
platon4ez <timoshech0303@gmail.com>
2023-12-24 22:31:41 +0500

    Код выводящий название архива

--- main.cpp
+++ main.cpp
@@ -1,42 +1,78 @@
 #include <iostream>
-#include <vector>
-#include <windows.h>
-using namespace std;
+#include <Windows.h>
+
+#pragma pack(push,1)
+
+struct RARHeaderData
+{
+    unsigned short HeadCRC;
+    unsigned char HeadType;
+    unsigned short Flags;
+    unsigned int PackSize;
+    unsigned int UnpSize;
+    unsigned int HostOS;
+    unsigned int FileCRC;
+    unsigned int FileTime;
+    unsigned int UnpVer;
+    unsigned int Method;
+    unsigned short NameSize;
+    unsigned int FileAttr;
+};
+
+struct RARHeaderDataEx
+{
+    RARHeaderData HeaderData;
+    char ArcName[260];
+    char FileName[260];
+    unsigned int RedirType;
+    char RedirName[260];
+};
+
+#pragma pack(pop)
+
+void archivec (unsigned char* data, int size)
+{
+    unsigned char* ptr = data;
+
+    while (ptr < data + size)
+    {
+        RARHeaderDataEx* header = (RARHeaderDataEx*)ptr;
+
+        if (header->HeaderData.HeadType == 0x74) // FILE_HEAD
+        {
+            std::cout << header->FileName << std::endl;
+        }
+
+        ptr += header->HeaderData.PackSize + sizeof(RARHeaderData);
+    }
+}
+
 int main()
 {
-	// Открываем архив
-	setlocale(LC_ALL, "Russian");
-	const char* archiveName = "archive.rar.rar";
-	HANDLE hFile = CreateFile(archiveName, GENERIC_READ, FILE_SHARE_READ, NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
-	if (hFile == INVALID_HANDLE_VALUE)
-	{
-		cerr << "Ошибка открытия архива. Пожалуйста, попробуйте снова" << endl;
-		return 1;
-	}
-	// Подсчитываем размер архива
-	DWORD archiveSize = GetFileSize(hFile, NULL);
-	if (archiveSize == INVALID_FILE_SIZE)
-	{
-		cerr << "Ошибка подсчёта размера архива" << endl;
-		CloseHandle(hFile);
-		return 1;
-	}
-	// Выводим размер архива
-	cout << "Размер архива: " << archiveSize << " байт" << endl;
-	// Добавляем информацию о размере архива в массив buffer типа vector
-	vector<char> buffer(archiveSize);
-	// Проверяем, успешно ли добавлена информация о размере архива в массив buffer
-	DWORD bytesRead;
-	if (!ReadFile(hFile, &buffer[0], archiveSize, &bytesRead, NULL))
-	{
-		cerr << "Ошибка чтения архива" << endl;
-		CloseHandle(hFile);
-		return 1;
-	}
+    HANDLE file = CreateFile("archive.rar", GENERIC_READ, FILE_SHARE_READ, NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
 
-	CloseHandle(hFile);
+    if (file == INVALID_HANDLE_VALUE)
+    {
+        std::cout << "Failed to open file" << std::endl;
+        return 1;
+    }
 
-	// Далее можно выполнять необходимые дальнейшие действия над данными, поскольку мы не удаляем значения из массива buffer
+    DWORD size = GetFileSize(file, NULL);
+    unsigned char* buffer = new unsigned char[size];
 
-	return 0;
+    DWORD bytesRead;
+    if (!ReadFile(file, buffer, size, &bytesRead, NULL))
+    {
+        std::cout << "Failed to read file" << std::endl;
+        CloseHandle(file);
+        delete[] buffer;
+        return 1;
+    }
+
+    archivec(buffer, size);
+
+    CloseHandle(file);
+    delete[] buffer;
+
+    return 0;
 }
